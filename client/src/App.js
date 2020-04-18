e5437156b755e061b409f3c3e3d6bf77618e15e8
Michelle Hey <miscmiche@gmail.com>
2020-04-17 20:54:26 -0400

    API call w/class component progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [card, setCard] = useState({});
+  
+  useEffect(() => {
+      fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then((card) => setCard(card));
+      console.log(res.json)
+      // return a clean-up function in order to debug terminal, cannot do setCard on an un-mounted component
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Name</h1>
+      <p className="card-name">{card.name}</p>
     </div>
   );
 }
